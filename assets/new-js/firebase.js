c0d971e4e763bc6533f476ff8619b05c6e22942f
Matheus Augusto Santos Martinelli <44898398+MartinelliNTZ@users.noreply.github.com>
2022-10-27 09:59:11 -0300

    last of day

---
+++ assets/new-js/firebase.js
@@ -0,0 +1,23 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDi2vJi0UNBDv6AJnfim-sqfVyWqdDkEQE",
+    authDomain: "copa-app-74786.firebaseapp.com",
+    projectId: "copa-app-74786",
+    storageBucket: "copa-app-74786.appspot.com",
+    messagingSenderId: "537153621249",
+    appId: "1:537153621249:web:bfe1000cd72eab4c339f00",
+    measurementId: "G-98K23JPWP7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+ 
+  export default app
--- assets/new-js/main.js
+++ assets/new-js/main.js
@@ -1,9 +1,10 @@
+import app  from './firebase.js'
 
 /**Document.get elemente pega o id do que foi atribuido ao item pela marcação html*/
 function showConfet(id){        
         const element = document.getElementById(id)
         party.confetti(element)
-        console.log('Axo que foi')
+        console.log(app)
 }
 
 /**O set interval é uma função que se repete varias vezes 5000=5s */
