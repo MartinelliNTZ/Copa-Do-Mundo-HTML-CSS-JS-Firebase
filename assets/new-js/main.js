3383e01b5a070ccfb1d25f4a9abec1531405c066
Matheus Augusto Santos Martinelli <44898398+MartinelliNTZ@users.noreply.github.com>
2022-10-26 23:28:14 -0300

    teste

--- assets/new-js/main.js
+++ assets/new-js/main.js
@@ -1,6 +1,13 @@
 
-function showConfet(id){
-    party.confetti(runButton, {
-        count: party.variation.range(20, 40),
-    });
+/**Document.get elemente pega o id do que foi atribuido ao item pela marcação html*/
+function showConfet(id){        
+        const element = document.getElementById(id)
+        party.confetti(element)
+        console.log('Axo que foi')
 }
+
+/**O set interval é uma função que se repete varias vezes 5000=5s */
+setInterval(() => {
+
+    showConfet('br-01')
+},1000)
