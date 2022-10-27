7b7ed6c98e173598083bfaa7cc28d8fa72c13915
Matheus Augusto Santos Martinelli <44898398+MartinelliNTZ@users.noreply.github.com>
2022-10-27 14:28:34 -0300

    finish

--- assets/new-js/main.js
+++ assets/new-js/main.js
@@ -12,6 +12,7 @@ onSnapshot(doc(db, 'matches', "br-01"), (doc) => {
     if(br>brScore){
         showConfet("br-01")
         throwGalvao()
+        showEmoji()
         brScore = brScore+1;
         console.log("Current data: ", doc.data());
     }
@@ -25,9 +26,16 @@ function throwGalvao(){
 function updateScore(id, br, other){
     const element = document.getElementById(id);
     element.innerText = `${br} x ${other}`
+    
 }
 
-
+function showEmoji(){
+    const emoji = document.getElementById('emoji-gol');
+    emoji.classList.add('show')
+    setTimeout(()=>{
+        emoji.classList.remove('show')
+    },4000)
+}
 
 /**Document.get elemente pega o id do que foi atribuido ao item pela marcação html*/
 function showConfet(id){        
@@ -38,6 +46,6 @@ function showConfet(id){
 
 /**O set interval é uma função que se repete varias vezes 5000=5s */
 setInterval(() => {
-
+    
     
 },1000)
