9fa2025e637dae7838b758ada27c0b6129579431
Matheus Augusto Santos Martinelli <44898398+MartinelliNTZ@users.noreply.github.com>
2022-10-27 12:24:44 -0300

    feito intalação do som e conexão com firebase

--- assets/new-js/firebase.js
+++ assets/new-js/firebase.js
@@ -19,5 +19,6 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  
  
   export default app
--- assets/new-js/main.js
+++ assets/new-js/main.js
@@ -1,14 +1,43 @@
 import app  from './firebase.js'
+import { getFirestore, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
+
+const db = getFirestore(app);
+ var brScore = 0;
+
+onSnapshot(doc(db, 'matches', "br-01"), (doc) => {
+    const {br, sv} = doc.data()
+    const other = sv;
+    updateScore("br-01", br, other)
+
+    if(br>brScore){
+        showConfet("br-01")
+        throwGalvao()
+        brScore = brScore+1;
+        console.log("Current data: ", doc.data());
+    }
+   
+});
+function throwGalvao(){
+    const audio =new Audio('assets/audio/gol.mp3')
+    audio.playbackRate = 2
+  //  audio.play()
+}
+function updateScore(id, br, other){
+    const element = document.getElementById(id);
+    element.innerText = `${br} x ${other}`
+}
+
+
 
 /**Document.get elemente pega o id do que foi atribuido ao item pela marcação html*/
 function showConfet(id){        
         const element = document.getElementById(id)
         party.confetti(element)
-        console.log(app)
+        
 }
 
 /**O set interval é uma função que se repete varias vezes 5000=5s */
 setInterval(() => {
 
-    showConfet('br-01')
+    
 },1000)
